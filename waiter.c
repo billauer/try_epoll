069d1879d8c9b525b934d2ded3bc0c6673812f5d
Eli Billauer <eli.billauer@gmail.com>
2020-01-15 12:21:42 +0200

    waiter.c: Open epoll file descriptor

--- waiter.c
+++ waiter.c
@@ -10,11 +10,24 @@
 #include <sys/epoll.h>
 
 int main(int argc, char **argv) {
+  int pollfd;
 
   unlink("fifo_for_test"); // Remove if exists. May fail, don't care.
 
   if (mkfifo("fifo_for_test", 0666)) {
     perror("mkfifo");
+    return 1;
+  }
+
+  pollfd = epoll_create1(0);
+
+  if (pollfd < 0) {
+    perror("epoll_create1");
+    return 1;
+  }
+
+  if (close(pollfd)) {
+    perror("While closing poll fd");
     return 1;
   }
 
