80d90759d631c320ff47932c9f277cc2c4f6793f
Eli Billauer <eli.billauer@gmail.com>
2020-01-15 11:51:23 +0200

    Add empty waiter.c

---
+++ waiter.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+
+  return 0;
+}
